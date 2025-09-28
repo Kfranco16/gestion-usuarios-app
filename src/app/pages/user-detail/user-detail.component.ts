c88ee8846bdc0df4035d63f0650ac4e38faa8a3a
Kevin Franco <arlex9511@gmail.com>
2025-09-28 19:47:22 +0200

    detalle del usuario y diseno mejorado

--- src/app/pages/user-detail/user-detail.component.ts
+++ src/app/pages/user-detail/user-detail.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
-
+// src/app/components/user-detail/user-detail.component.ts
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs';
+import { UserService } from '../../services/user.service';
+import { IUser } from '../../interfaces/iuser.interface';
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // <-- 1. Importamos RouterLink para el botón de "Volver"
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.css'
+  styleUrl: './user-detail.component.css',
 })
 export class UserDetailComponent {
+  // 2. Inyectamos las dependencias que necesitamos
+  private route = inject(ActivatedRoute);
+  private userService = inject(UserService);
 
+  // 3. El NÚCLEO REACTIVO: creamos un signal a partir de los parámetros de la ruta
+  public user = toSignal(
+    this.route.paramMap.pipe(
+      // a. Obtenemos el parámetro 'id' de la URL
+      map((params) => params.get('_id')),
+      // b. Nos aseguramos de que el ID no sea nulo o undefined
+      filter((id): id is string => !!id),
+      // c. Usamos switchMap para llamar al servicio con el ID y obtener los datos del usuario
+      switchMap((id) => this.userService.getUserById(id))
+    )
+  );
 }
